d7a42b05ffe281503403fe5431807b818d52d709
ToBinio <tobias.frischmann1@gmail.com>
2022-10-30 09:10:28 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+mod container;
+
+use std::sync::Mutex;
+use actix_files::{Files, NamedFile};
+use actix_web::{App, get, HttpServer, Result};
+use actix_web::middleware::Logger;
+use actix_web::web::Data;
+use env_logger::Env;
+use crate::container::{add_container, Containers, get_all_containers};
+
+#[get("/")]
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open_async("./public/dist/public/index.html").await?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    let containers = Data::new(Containers {
+        containers: Mutex::new(Vec::new())
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .app_data(containers.clone())
+            .service(index)
+            .service(get_all_containers)
+            .service(add_container)
+            .service(Files::new("/", "./public/dist/public").show_files_listing())
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
